f3a744907938263eda97d4a5d139c0c38b1d032a
Ronald van der Bergh <rhvdbergh@gmail.com>
2019-12-21 19:49:09 -0600

    add textbox

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import TextBox from './TextBox';
 
 const QuoteBox = () => {
-  return <div id="quote-box"></div>;
+  return (
+    <div id="quote-box">
+      <TextBox />
+    </div>
+  );
 };
 
 export default QuoteBox;
---
+++ src/components/TextBox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TextBox = () => {
+  return <h1 id="text"></h1>;
+};
+
+export default TextBox;
