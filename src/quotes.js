62b02bdad45b50f35dedd0aac2d58160bc5796e6
Ronald van der Bergh <rhvdbergh@gmail.com>
2019-12-21 21:56:05 -0600

    add mock quotes and import

---
+++ src/quotes.js
@@ -0,0 +1,16 @@
+const quotes = [
+  {
+    author: 'Author One',
+    quote: 'Inspiring Quote 1'
+  },
+  {
+    author: 'Author Two',
+    quote: 'Inspiring Quote 2'
+  },
+  {
+    author: 'Author Three',
+    quote: 'Inspiring Quote 3'
+  }
+];
+
+export default quotes;
