b019df3b9dbf98c41ba00f514cee211d29ad23d1
Ronald van der Bergh <rhvdbergh@gmail.com>
2019-12-23 00:52:19 -0600

    small style change, Shakespeare quotes

--- src/quotes.js
+++ src/quotes.js
@@ -1,19 +1,26 @@
 const quotes = [
   {
-    author: 'Author One',
-    quote: 'Inspiring Quote 1'
+    author: 'Cleopatra (Shakespeare, Antony and Cleopatra II, 5)',
+    quote: 'Give me some music; music, moody food of us that trade in love.'
   },
   {
-    author: 'Author Two',
-    quote: 'Inspiring Quote 2'
+    author: 'Orlando (Shakespeare, As You Like It II, 7)',
+    quote: 'I almost die for food, and let me have it.'
   },
   {
-    author: 'Author Three',
-    quote: 'Inspiring Quote 3'
+    author: "Clown (Shakespeare, All's Well That Ends Well III, 2)",
+    quote:
+      'Why, he will look upon his boot and sing; mend the ruff and sing; ask questions and sing; pick his teeth and sing. I know a man that had this trick of melancholy sold a goodly manor for a song.'
   },
   {
-    author: 'Author Four',
-    quote: 'Inspiring Quote 4'
+    author: 'Jaques (Shakespeare, As You Like It II, 5)',
+    quote:
+      'I thank it. More, I prithee, more. I can suck melancholy out of a song, as a weasel sucks eggs. More, I prithee, more.'
+  },
+  {
+    author: 'Celia (Shakespeare, As You Like It III, 2)',
+    quote:
+      "I would sing my song without a burden; thou bring'st me out of tune."
   }
 ];
 
